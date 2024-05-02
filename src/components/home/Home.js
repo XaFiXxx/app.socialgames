3e5bfe2774013879906d52f39456bfbe1365c6bb
Benjamin Schroeders <benjamin.schroeders@icloud.com>
2024-05-02 21:14:01 +0200

    Correctifs home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,10 +3,10 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PostForm from "../posts/PostForm";
-import Post from "../posts/Posts";
+import Posts from "../posts/Posts"; // Assurez-vous que c'est le bon import et que le nom est correct
 
 function Home() {
-  const [posts, setPosts] = useState([]); // État pour stocker les posts
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -15,16 +15,19 @@ function Home() {
         const response = await axios.get("http://localhost:8000/api/home", {
           headers: { Authorization: `Bearer ${token}` },
         });
-        setPosts(response.data); // Stocker les posts dans l'état
-        console.log(response.data); // Afficher les données dans la console pour vérifier
+        setPosts(response.data);
+        console.log(response.data);
       } catch (error) {
         console.error("Erreur lors de la récupération des posts:", error);
         toast.error("Impossible de récupérer les posts.");
       }
     };
+    fetchPosts();
+  }, []);
 
-    fetchPosts(); // Appel de la fonction au montage du composant
-  }, []); // Le tableau vide assure que l'effet s'exécute une seule fois au montage
+  const handleNewPost = (newPost) => {
+    setPosts((prevPosts) => [newPost, ...prevPosts]);
+  };
 
   return (
     <div className="bg-gray-800 text-gray-200 min-h-screen">
@@ -34,11 +37,14 @@ function Home() {
           Le réseau social pour les passionnés de jeux vidéo.
         </p>
       </section>
-      <PostForm />
+
+      <div className="mt-4 w-full max-w-4xl mx-auto ">
+        <PostForm onPostSubmit={handleNewPost} />
+      </div>
 
       <section className="flex flex-col items-center mb-8">
         <h2 className="text-3xl font-bold mt-8">Derniers posts</h2>
-        <Post posts={posts} />
+        <Posts posts={posts} />
       </section>
     </div>
   );
