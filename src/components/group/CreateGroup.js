d254d074c732d66912623d25512f12fee3115494
Benjamin Schroeders <benjamin.schroeders@icloud.com>
2024-05-14 18:00:43 +0200

    Optimisation create groupes et carrousel

--- src/components/group/CreateGroup.js
+++ src/components/group/CreateGroup.js
@@ -34,68 +34,89 @@ const CreateGroup = ({ games, onCreateGroup, onClose }) => {
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white p-4 rounded shadow-lg">
-        <h2 className="text-xl font-bold mb-4">Créer un nouveau groupe</h2>
+    <div className="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center p-4">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg w-full max-w-lg">
+        <h2 className="text-2xl font-bold mb-6 text-white text-center">
+          Créer un nouveau groupe
+        </h2>
         <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className="border p-2 mb-4 w-full"
-            placeholder="Nom du groupe"
-            value={newGroupName}
-            onChange={(e) => setNewGroupName(e.target.value)}
-          />
-          <input
-            type="text"
-            className="border p-2 mb-4 w-full"
-            placeholder="Description du groupe"
-            value={newGroupDescription}
-            onChange={(e) => setNewGroupDescription(e.target.value)}
-          />
-          <select
-            className="border p-2 mb-4 w-full"
-            value={newGroupGameId}
-            onChange={(e) => setNewGroupGameId(e.target.value)}
-          >
-            <option value="">Sélectionner un jeu</option>
-            {games.map((game) => (
-              <option key={game.id} value={game.id}>
-                {game.name}
-              </option>
-            ))}
-          </select>
-          <input
-            type="file"
-            accept="image/*"
-            className="border p-2 mb-4 w-full"
-            onChange={handleImageChange}
-          />
-          {newGroupImagePreview && (
-            <img
-              src={newGroupImagePreview}
-              alt="Aperçu"
-              className="w-full h-32 object-cover rounded mb-4"
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">Nom du groupe</label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 border border-gray-700 rounded-lg bg-gray-900 text-white"
+              placeholder="Nom du groupe"
+              value={newGroupName}
+              onChange={(e) => setNewGroupName(e.target.value)}
             />
-          )}
-          <select
-            className="border p-2 mb-4 w-full"
-            value={newGroupPrivacy}
-            onChange={(e) => setNewGroupPrivacy(e.target.value)}
-          >
-            <option value="public">Public</option>
-            <option value="private">Privé</option>
-          </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">
+              Description du groupe
+            </label>
+            <input
+              type="text"
+              className="w-full px-4 py-2 border border-gray-700 rounded-lg bg-gray-900 text-white"
+              placeholder="Description du groupe"
+              value={newGroupDescription}
+              onChange={(e) => setNewGroupDescription(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">Jeu associé</label>
+            <select
+              className="w-full px-4 py-2 border border-gray-700 rounded-lg bg-gray-900 text-white"
+              value={newGroupGameId}
+              onChange={(e) => setNewGroupGameId(e.target.value)}
+            >
+              <option value="">Sélectionner un jeu</option>
+              {games.map((game) => (
+                <option key={game.id} value={game.id}>
+                  {game.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">Image de groupe</label>
+            <input
+              type="file"
+              accept="image/*"
+              className="w-full px-4 py-2 border border-gray-700 rounded-lg bg-gray-900 text-white"
+              onChange={handleImageChange}
+            />
+            {newGroupImagePreview && (
+              <img
+                src={newGroupImagePreview}
+                alt="Aperçu"
+                className="w-full h-32 object-cover rounded-lg mt-2"
+              />
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-300 mb-2">Confidentialité</label>
+            <select
+              className="w-full px-4 py-2 border border-gray-700 rounded-lg bg-gray-900 text-white"
+              value={newGroupPrivacy}
+              onChange={(e) => setNewGroupPrivacy(e.target.value)}
+            >
+              <option value="public">Public</option>
+              <option value="private" disabled>
+                Privé (Indisponible)
+              </option>
+            </select>
+          </div>
           <div className="flex justify-end">
             <button
               type="button"
-              className="px-4 py-2 bg-gray-500 text-white rounded mr-2"
+              className="px-4 py-2 bg-red-600 text-white rounded-lg mr-2 hover:bg-red-700"
               onClick={onClose}
             >
               Annuler
             </button>
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-500 text-white rounded"
+              className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700"
             >
               Créer
             </button>
--- src/components/home/GameCarousel.js
+++ src/components/home/GameCarousel.js
@@ -8,39 +8,46 @@ import { Link } from "react-router-dom";
 
 function GameCarousel({ games }) {
   return (
-    <Swiper
-      spaceBetween={30}
-      slidesPerView={3}
-      modules={[Navigation, Pagination]}
-      navigation
-      pagination={{ clickable: true }}
-      loop={true}
-      className="mySwiper w-full max-w-4xl mx-auto"
-    >
-      {games.map((game, index) => (
-        <SwiperSlide key={index}>
-          <div className="bg-gray-800 p-4 rounded-lg shadow-lg overflow-hidden">
-            <img
-              src={`http://localhost:8000/${game.cover_image}`} // Vérifie l'exactitude du chemin
-              alt={game.name}
-              className="w-full h-64 object-cover"
-            />
-            <div className="p-4">
-              <h3 className="text-lg font-bold text-white">{game.name}</h3>
-              <p className="text-gray-300 truncate">{game.description}</p>
-              <div className="mt-4 text-center btn btn-blue">
-                <Link
-                  to={`/game/${game.id}/${game.name}`}
-                  className="text-gray-200 hover:underline mt-2 block"
-                >
-                  View Details
-                </Link>
+    <div className="relative w-full max-w-6xl mx-auto">
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        modules={[Navigation, Pagination]}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        pagination={{ clickable: true }}
+        loop={true}
+        className="mySwiper"
+      >
+        {games.map((game, index) => (
+          <SwiperSlide key={index}>
+            <div className="bg-gray-800 p-4 rounded-lg shadow-lg overflow-hidden">
+              <img
+                src={`http://localhost:8000/${game.cover_image}`} // Vérifie l'exactitude du chemin
+                alt={game.name}
+                className="w-full h-64 object-cover"
+              />
+              <div className="p-4">
+                <h3 className="text-lg font-bold text-white">{game.name}</h3>
+                <p className="text-gray-300 truncate">{game.description}</p>
+                <div className="mt-4 text-center">
+                  <Link
+                    to={`/game/${game.id}/${game.name}`}
+                    className="text-gray-200 hover:underline mt-2 block"
+                  >
+                    View Details
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="swiper-button-prev absolute left-4 top-1/2 transform -translate-y-1/2 btn-blue text-white p-2 min-h-14 rounded-full z-11 hover:bg-gray-600"></div>
+      <div className="swiper-button-next absolute right-4 top-1/2 transform -translate-y-1/2 btn-blue text-white p-2 min-h-14 rounded-full z-11 hover:bg-gray-600"></div>
+    </div>
   );
 }
 
